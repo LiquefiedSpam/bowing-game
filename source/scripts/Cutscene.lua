aadf48f63ec9e76ec9b40e96ceb779bc63ad314b
Tris Wong <tristonwongis@gmail.com>
2026-07-18 15:02:44 +0900

    cutscenes added

---
+++ source/scripts/Cutscene.lua
@@ -0,0 +1,33 @@
+class("Cutscene").extends()
+
+local gfx = playdate.graphics
+local pd = playdate
+local boxes_sprite = gfx.image.new("images/background/box-sprites.png")
+
+function Cutscene:init(image_one_path, image_two_path, image_three_path)
+    self.image_one_path = gfx.image.new(image_one_path)
+    self.image_two_path = gfx.image.new(image_two_path)
+    self.image_three_path = gfx.image.new(image_three_path)
+    self.animation_complete = false
+end
+
+function Cutscene:draw()
+    if not self.animation_complete then
+        self:animation_sequence()
+        self.animation_complete = true
+    end
+    boxes_sprite:draw(0, 0)
+    self.image_one_path:draw(17, 64)
+    self.image_two_path:draw(145, 64)
+    self.image_three_path:draw(273, 64)
+end
+
+function Cutscene:animation_sequence()
+    boxes_sprite:draw(0, 0)
+    pd.wait(1000)
+    self.image_one_path:draw(17, 64)
+    pd.wait(1000)
+    self.image_two_path:draw(145, 64)
+    pd.wait(1000)
+    self.image_three_path:draw(273, 64)
+end
