58830302ef6d1d3e7ebdff24c94486cceaac0281
LiquefiedSpam <morganjwillsjr@gmail.com>
2026-07-20 11:16:50 +0900

    stuff

--- source/scripts/CharacterSprite.lua
+++ source/scripts/CharacterSprite.lua
@@ -77,7 +77,7 @@ function CharacterSprite:startWalkIn(walkIn, isPlayer)
     self.hasWalkedIn = false
     self.startedWalkingIn = true
     self.walking = true
-    self.baseY = 200
+    self.baseY = 240
     self.bouncePhase = 0
     self.bounceAmplitude = 4   --small bounce
     self.bounceFrequency = 0.3 --how many 'bounces' per pixel walked
